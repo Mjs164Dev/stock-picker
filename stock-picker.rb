95d76b55c4ff8f2a951cad9ff7807a023ef656f2
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-08 17:24:49 -0500

    set up basic program framework.

---
+++ stock-picker.rb
@@ -0,0 +1,13 @@
+stock_prices = [12,33,35,1,23,5]
+
+def stock_picker(stock_prices)
+  # find lowest number in array & index
+  # find highest number in array & index
+  # find the difference in price
+
+  # compute each possible buy/sell scenario by looping through the array.
+
+  # 
+end
+
+stock_picker(stock_prices)
