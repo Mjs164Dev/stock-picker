67678fae9e909e6d37fa7c3e4d7d75de827a8f4e
Mike Schmid <152652501+Mjs164Dev@users.noreply.github.com>
2024-02-08 21:28:23 -0500

    crush the whole program in one go.

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,13 +1,22 @@
-stock_prices = [12,33,35,1,23,5]
+stock_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(stock_prices)
-  # find lowest number in array & index
-  # find highest number in array & index
-  # find the difference in price
+  largest_gap = 0
+  buy_price_index = 0
+  sell_price_index = 0
 
   # compute each possible buy/sell scenario by looping through the array.
-
-  # 
+  stock_prices.each_with_index do |price, index|
+    for i in index..stock_prices.length - 1
+      current_gap = stock_prices[i] - price
+      if current_gap > largest_gap
+        largest_gap = current_gap
+        buy_price_index = index
+        sell_price_index = i
+      end
+    end
+  end
+  puts "Buy on day #{buy_price_index}, Sell on day: #{sell_price_index}"
 end
 
 stock_picker(stock_prices)
